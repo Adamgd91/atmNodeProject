d049e45935cf1fc7a712ded48642e5f25a52c61a
Adam Davidson <agdavidson91@gmail.com>
2022-03-10 20:01:04 -0600

    Added the functions in the atm.js file

---
+++ atm.js
@@ -0,0 +1,15 @@
+function getBalance() {
+  console.log(`Your Balance is`);
+}
+
+function withdraw() {
+  console.log(`Your withdrew is`);
+}
+
+function deposit() {
+  console.log(`Your Deposit is`);
+}
+
+function validatePin() {
+  console.log(`Your Pin is`);
+}
